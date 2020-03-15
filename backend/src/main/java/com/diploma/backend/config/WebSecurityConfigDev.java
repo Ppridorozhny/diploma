ff4b966ff7e3e5bb9799981e4fa751dd38327439
Petro Prydorozhnyi <petro.prydorozhnyi@gmail.com>
2020-03-15 15:53:50 +0200

    fixed security stub

--- backend/src/main/java/com/diploma/backend/config/WebSecurityConfigDev.java
+++ backend/src/main/java/com/diploma/backend/config/WebSecurityConfigDev.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -22,5 +24,11 @@ public class WebSecurityConfigDev extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
