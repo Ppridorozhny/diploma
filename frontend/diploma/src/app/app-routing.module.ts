89b692c0957b9be8497c047b47cdc6a5f6fe313e
PPrydorozhnyi <Peter35128@gmail.com>
2020-05-01 06:25:14 +0300

    added routes

--- frontend/diploma/src/app/app-routing.module.ts
+++ frontend/diploma/src/app/app-routing.module.ts
@@ -3,18 +3,25 @@ import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {HomeComponent} from "./home/home.component";
 import {AuthGuard} from "./service/auth.guard";
+import {TicketAddComponent} from "./ticket/ticket.add/ticket.add.component";
+import {TicketComponent} from "./ticket/ticket/ticket.component";
+import {TicketEditComponent} from "./ticket/ticket.edit/ticket.edit.component";
 
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
-  { path: 'login', component: LoginComponent },
+  {path: '', component: HomeComponent, canActivate: [AuthGuard]},
+  {path: 'project/:projectId/ticket', component: TicketAddComponent, canActivate: [AuthGuard]},
+  {path: 'project/:projectId/ticket/:ticketId', component: TicketComponent, canActivate: [AuthGuard]},
+  {path: 'project/:projectId/ticket/:ticketId/edit', component: TicketEditComponent, canActivate: [AuthGuard]},
+  {path: 'login', component: LoginComponent},
 
   // redirect to home
-  { path: '**', redirectTo: '' }
+  {path: '**', redirectTo: ''}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
